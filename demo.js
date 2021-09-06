1d592157cb063549ae0365e5551050cf8a60e1c1
Fred-shen <fred-shen@Fred-shendeMacBook-Pro.local>
2021-09-07 00:14:06 +0800

    chore: bootstrap tooltip popover

--- demo.js
+++ demo.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
   console.log(ajaxobj);
   ajaxobj.getall();
 
-  // 新增按鈕
-  var exampleModal = document.getElementById('exampleModal')
+  // 搜尋按鈕
+  var exampleModal = document.getElementById('addbtn')
   exampleModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget
@@ -53,50 +53,50 @@ $(document).ready(function () {
   //     });
   // })
   // 搜尋按鈕
-  $("#searchbutton").click(function () {
-      $("#dialog-searchconfirm").dialog({
-          resizable: true,
-          height: $(window).height() * 0.4,// dialog視窗度
-          width: $(window).width() * 0.4,
-          modal: true,
-          buttons: {
-              // 自訂button名稱
-              "搜尋": function (e) {
-                  var url = "ajax/ajaxCard";
-                  // var data = $("#searchform").serialize();
-                  var cnname = $("#secnname").val();
-                  var enname = $("#seenname").val();
-                  var sex = $('input:radio:checked[name="sesex"]').val();
-                  var ajaxobj = new AjaxObject(url, 'json');
-                  ajaxobj.cnname = cnname;
-                  ajaxobj.enname = enname;
-                  ajaxobj.sex = sex;
-                  ajaxobj.search();
+  // $("#searchbutton").click(function () {
+  //     $("#dialog-searchconfirm").dialog({
+  //         resizable: true,
+  //         height: $(window).height() * 0.4,// dialog視窗度
+  //         width: $(window).width() * 0.4,
+  //         modal: true,
+  //         buttons: {
+  //             // 自訂button名稱
+  //             "搜尋": function (e) {
+  //                 var url = "ajax/ajaxCard";
+  //                 // var data = $("#searchform").serialize();
+  //                 var cnname = $("#secnname").val();
+  //                 var enname = $("#seenname").val();
+  //                 var sex = $('input:radio:checked[name="sesex"]').val();
+  //                 var ajaxobj = new AjaxObject(url, 'json');
+  //                 ajaxobj.cnname = cnname;
+  //                 ajaxobj.enname = enname;
+  //                 ajaxobj.sex = sex;
+  //                 ajaxobj.search();
 
-                  e.preventDefault(); // avoid to execute the actual submit of the form.
-              },
-              "重新填寫": function () {
+  //                 e.preventDefault(); // avoid to execute the actual submit of the form.
+  //             },
+  //             "重新填寫": function () {
 
-                  $("#searchform")[0].reset();
-              },
-              "取消": function () {
-                  $(this).dialog("close");
-              }
-          }
-      });
-  })
+  //                 $("#searchform")[0].reset();
+  //             },
+  //             "取消": function () {
+  //                 $(this).dialog("close");
+  //             }
+  //         }
+  //     });
+  // })
   // 修改鈕
-  $("#cardtable").on('click', '.modifybutton', function () {
-      var ajaxobj = new AjaxObject(url, 'json');
-      ajaxobj.modify_get();
-  })
-  $("#cardtable").on('click', '.deletebutton', function () {
-      var deleteid = $(this).attr('id').substring(12);
-      var url = "ajax/ajaxCard";
-      var ajaxobj = new AjaxObject(url, 'json');
-      ajaxobj.id = deleteid;
-      ajaxobj.delete();
-  })
+  // $("#cardtable").on('click', '.modifybutton', function () {
+  //     var ajaxobj = new AjaxObject(url, 'json');
+  //     ajaxobj.modify_get();
+  // })
+  // $("#cardtable").on('click', '.deletebutton', function () {
+  //     var deleteid = $(this).attr('id').substring(12);
+  //     var url = "ajax/ajaxCard";
+  //     var ajaxobj = new AjaxObject(url, 'json');
+  //     ajaxobj.id = deleteid;
+  //     ajaxobj.delete();
+  // })
 
   // 自適應視窗
   $(window).resize(function () {
@@ -118,15 +118,24 @@ function refreshTable(data) {
           strsex = '男';
       else
           strsex = '女';
+      let tooltipString = `[${strsex}]${item.cnname}(${item.enname})`
+      let popoverString = `聯絡方式 ： ${conversePhone(item.phone)}`
+      console.log(popoverString);
       var row = $("<tr></tr>");
-      row.append($("<td></td>").html(item.cnname));
+      row.append($(`<td data-original-title="111" data-toggle="tooltip" data-placement="bottom" class="matrisHeader" data-container="body" title="${tooltipString}"></td>`).html(item.cnname));
       row.append($("<td></td>").html(item.enname));
       row.append($("<td></td>").html(strsex));
-      row.append($("<td></td>").html(item.phone));
+      row.append($(`<td data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="${popoverString}"></td>`).html(item.phone));
       row.append($("<td></td>").html(item.email));
       row.append($("<td></td>").html('<button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modify" data-bs-whatever="@mdo">修改</button>'));
       row.append($("<td></td>").html('<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete" data-bs-whatever="@mdo">刪除</button>'));
       $("#cardtable").append(row);
+      var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+      var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+      })
+      console.log(popoverList);
+      $("[data-toggle='tooltip']").tooltip();
   });
 }
 
@@ -204,7 +213,7 @@ response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0","phone
 refreshTable(JSON.parse(response));
 }
 AjaxObject.prototype.add = function () {
-response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"},{"s_sn":"52","cnname":"新增帳號","enname":"NewAccount","sex":"1"}]';
+response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0","phone":"0938337898","email":"abc@gmail.com"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0","phone":"0918301738","email":"cdn@gmail.com"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0","phone":"0921387390","email":"btn@gmail.com"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1","phone":"0932357198","email":"mdn@gmail.com"},{"s_sn":"52","cnname":"新增帳號","enname":"NewAccount","sex":"1"}]';
 refreshTable(JSON.parse(response));
 $("#dialog-addconfirm").dialog("close");
 }
@@ -227,85 +236,87 @@ response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_s
 refreshTable(JSON.parse(response));
 }
   
-document.querySelector('#email').addEventListener('blur', validateEmail);
+// document.querySelector('#email').addEventListener('blur', validateEmail);
 
-document.querySelector('#password').addEventListener('blur', validatePassword);
+// document.querySelector('#password').addEventListener('blur', validatePassword);
 
-document.querySelector('#username').addEventListener('blur', validateUsername);
+// document.querySelector('#username').addEventListener('blur', validateUsername);
 
-const reSpaces = /^\S*$/;
+// const reSpaces = /^\S*$/;
 
-function validateUsername(e) {
-  const username = document.querySelector('#username');
-  if (reSpaces.test(username.value)) {
-    username.classList.remove('is-invalid');
-    username.classList.add('is-valid');
-    return true;
-  } else {
-    username.classList.remove('is-valid');
+// function validateUsername() {
+//   const username = document.querySelector('#username');
+//   if (reSpaces.test(username.value)) {
+//     username.classList.remove('is-invalid');
+//     username.classList.add('is-valid');
+//     return true;
+//   } else {
+//     username.classList.remove('is-valid');
 
-    username.classList.add('is-invalid');
-    return false;
-  }
-}
+//     username.classList.add('is-invalid');
+//     return false;
+//   }
+// }
 
-function validateEmail(e) {
-  const email = document.querySelector('#email');
-  const re = /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z]){3,}\.([a-zA-Z]){2,5}$/;
+// function validateEmail() {
+//   const email = document.querySelector('#email');
+//   const re = /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z]){3,}\.([a-zA-Z]){2,5}$/;
 
-  if (reSpaces.test(email.value) && re.test(email.value)) {
-    email.classList.remove('is-invalid');
-    email.classList.add('is-valid');
+//   if (reSpaces.test(email.value) && re.test(email.value)) {
+//     email.classList.remove('is-invalid');
+//     email.classList.add('is-valid');
 
-    return true;
-  } else {
-    email.classList.add('is-invalid');
-    email.classList.remove('is-valid');
+//     return true;
+//   } else {
+//     email.classList.add('is-invalid');
+//     email.classList.remove('is-valid');
 
-    return false;
-  }
-}
+//     return false;
+//   }
+// }
 
-function validatePassword() {
-  const password = document.querySelector('#password');
-  const re = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})(?=.*[!@#$%^&*])/;
-  if (re.test(password.value) && reSpaces.test(password.value)) {
-    password.classList.remove('is-invalid');
-    password.classList.add('is-valid');
+// function validatePassword() {
+//   const password = document.querySelector('#password');
+//   const re = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})(?=.*[!@#$%^&*])/;
+//   if (re.test(password.value) && reSpaces.test(password.value)) {
+//     password.classList.remove('is-invalid');
+//     password.classList.add('is-valid');
 
-    return true;
-  } else {
-    password.classList.add('is-invalid');
-    password.classList.remove('is-valid');
+//     return true;
+//   } else {
+//     password.classList.add('is-invalid');
+//     password.classList.remove('is-valid');
 
-    return false;
-  }
-}
+//     return false;
+//   }
+// }
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  const forms = document.querySelectorAll('.needs-validation');
+  'use strict'
 
-  for (let form of forms) {
-    form.addEventListener(
-      'submit',
-      function (event) {
-        if (
-          !form.checkValidity() ||
-          !validateEmail() ||
-          !validateUsername() ||
-          !validatePassword()
-        ) {
-          event.preventDefault();
-          event.stopPropagation();
-        } else {
-          form.classList.add('was-validated');
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
         }
-      },
-      false
-    );
-  }
-})();
 
-$(function () {
-  $("[data-toggle='tooltip']").tooltip();
-});
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
+function conversePhone(n) {
+  let ary = n.split('');
+  ary.splice(4, 0, '-');
+  ary.splice(8, 0, '-');
+  let result = ary.join('');
+  return result;
+}
